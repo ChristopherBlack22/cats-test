f2e4d8c32331f10418491fb718d656685c69d126
Christopher Black <christopher.black.2209@googlemail.com>
2025-05-14 00:38:37 +0100

    Move message fetch to WelcomeCard Reduce prop drilling

--- frontend/src/components/WelcomeCard.tsx
+++ frontend/src/components/WelcomeCard.tsx
@@ -1,16 +1,24 @@
 import Link from "next/link";
+import { notFound } from 'next/navigation';
 import CommsCard from "./CommsCard";
 import catPic from '@public/images/Nara.jpg';
 import { formatGBP } from "@/utils/formatPrices";
 
-type WelcomeCardProps = {
-    title: string;
-    message: string;
-    totalPrice: number;
-    freeGift: boolean;
+async function getMessage (userId: string): Promise<{ title: string; message: string; totalPrice: number; freeGift: boolean; }> {
+    const res = await fetch(`http://localhost:3001/comms/your-next-delivery/${userId}`, {cache: 'no-store'});
+    if (!res.ok) {
+        notFound();
+    }
+    const welcomeMsg = await res.json();
+    return welcomeMsg;
 }
 
-const WelcomeCard = ({title, message, totalPrice, freeGift} : WelcomeCardProps) => {
+type WelcomeCardProps = {
+    userId: string;
+}
+
+const WelcomeCard = async ({ userId } : WelcomeCardProps) => {
+    const {title, message, totalPrice, freeGift} = await getMessage(userId);
 
     const priceTxt = `Total price: ${formatGBP(totalPrice)}`
 
